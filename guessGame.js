88eab9acd01f3e61305f14b3216cf0e17f7a7de0
Mamta poudel <m.yrx.mamata@gmail.com>
2026-02-02 21:49:15 +0545

    Have Fun

---
+++ guessGame.js
@@ -0,0 +1,8 @@
+let gameNum=49;
+let userNum=prompt("Enter a  number between 1 and 100:");
+while(userNum!=gameNum)
+{
+    userNum=prompt("Wrong number.Try again");
+    
+}
+console.log("Congrats!!! You've guessed it right");
